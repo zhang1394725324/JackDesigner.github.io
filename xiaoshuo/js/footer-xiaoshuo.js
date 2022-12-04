44ecdf8433bcf12aa39a3e5def7ba84a252d54ef
Jack.Zhang <53216337+zhang1394725324@users.noreply.github.com>
2022-12-05 00:21:05 +0800

    Add files via upload

---
+++ xiaoshuo/js/footer-xiaoshuo.js
@@ -0,0 +1,15 @@
+document.writeln("            <footer>");
+document.writeln("                <div class=\'row\'>");
+document.writeln("                    <div class=\'col-md-12\'>");
+document.writeln("                        <div class=\'social\'>");
+document.writeln("                            <a href=\'javascript:;\'><i class=\'fa fa-facebook\'></i></a>");
+document.writeln("                            <a href=\'javascript:;\'><i class=\'fa fa-twitter\'></i></a>");
+document.writeln("                            <a href=\'javascript:;\'><i class=\'fa fa-pinterest\'></i></a>");
+document.writeln("                            <a href=\'javascript:;\'><i class=\'fa fa-dribbble\'></i></a>");
+document.writeln("                        </div>");
+document.writeln("                        <p>");
+document.writeln("                            Copyright &copy; 2022.Company name All rights reserved.");
+document.writeln("                        </p>");
+document.writeln("                    </div>");
+document.writeln("                </div>");
+document.writeln("            </footer>");
