b7892af6b8532ff82839df845427c52d51adcb7a
Jack.Zhang <53216337+zhang1394725324@users.noreply.github.com>
2022-12-07 15:12:56 +0800

    Create system-date.js

---
+++ js/system-date.js
@@ -0,0 +1,10 @@
+function startTime(){
+  var today=new Date();
+  var h=today.getHours();
+  var m=today.getMinutes();
+  var s=today.getSeconds();//在小于10的数字前加一个‘0’
+  m=checkTime(m);
+  s=checkTime(s);
+  document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+  t=setTimeout(function(){startTime()},500);
+}
